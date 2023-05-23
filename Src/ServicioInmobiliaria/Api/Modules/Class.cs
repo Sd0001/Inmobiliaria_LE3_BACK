9e32b687dd9d0b4188a04ee3e6a596e25ab204f5
jesus David Arango <jesus.arango.restrepo@gmail.com>
2023-05-23 12:48:12 -0500

    ajuste espacios de nombres y maqueta de permisos

---
+++ Src/ServicioInmobiliaria/Api/Modules/Class.cs
@@ -0,0 +1,41 @@
+﻿using Azure.Core;
+using Azure;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Text.Encodings.Web;
+
+namespace Inmobiliaria.Api.Modules
+{
+    public class BasicAuthenticationHandler : IAuthenticationHandler
+    {
+        
+        public BasicAuthenticationHandler( ) 
+        {
+        }
+
+        public Task<AuthenticateResult> AuthenticateAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ChallengeAsync(AuthenticationProperties? properties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ForbidAsync(AuthenticationProperties? properties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InitializeAsync(AuthenticationScheme scheme, HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
